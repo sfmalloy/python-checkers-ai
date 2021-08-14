ee964a1ad47a4aa1be68beb5cebcd6cf70e1ad65
JohnHynes <johnthehynes@gmail.com>
2021-08-14 10:47:10 -0400

    First pass of game model

---
+++ board_model.py
@@ -0,0 +1,167 @@
+from enum import Enum
+
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class tile_state(Enum):
+    EMPTY    = 0
+    PLAYER_1 = 1
+    PLAYER_2 = 2
+
+class move_status(Enum):
+    FAILED   = 0
+    MOVED    = 1
+    CAPTURED = 2
+
+class tile:
+    def __init__ (self, state: tile_state):
+        self.state: tile_state = state
+        self.is_king: bool       = False
+
+class board:
+    def __init__(self):
+        self.grid = []
+        # init player 1
+        for i in range(3):
+            row = []
+            for j in range(8):
+                if i % 2 == 0:
+                    if j % 2 == 0:
+                        row.append(tile(tile_state.EMPTY))
+                    else:
+                        row.append(tile(tile_state.PLAYER_1))
+                else:
+                    if j % 2 == 0:
+                        row.append(tile(tile_state.PLAYER_1))
+                    else:
+                        row.append(tile(tile_state.EMPTY))
+            self.grid.append(row)
+        # init no mans land
+        for i in range(2):
+            row = []
+            for j in range(8):
+                row.append(tile(tile_state.EMPTY))
+            self.grid.append(row)
+        # init player 2
+        for i in range(3):
+            row = []
+            for j in range(8):
+                if i % 2 == 0:
+                    if j % 2 == 0:
+                        row.append(tile(tile_state.PLAYER_2))
+                    else:
+                        row.append(tile(tile_state.EMPTY))
+                else:
+                    if j % 2 == 0:
+                        row.append(tile(tile_state.EMPTY))
+                    else:
+                        row.append(tile(tile_state.PLAYER_2))
+            self.grid.append(row)
+                
+    
+    def print(self):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                print(self.grid[i][j].state.value, end=' ')
+            print()
+
+    
+    def is_legal_move(self, src: point, dest: point, player: tile_state) -> move_status:
+        # checking inputs
+        if (player == tile_state.EMPTY): return move_status.FAILED
+        if src.x < 0 or 7 < src.x: return move_status.FAILED
+        if src.y < 0 or 7 < src.y: return move_status.FAILED
+        if dest.x < 0 or 7 < dest.x: return move_status.FAILED
+        if dest.y < 0 or 7 < dest.y: return move_status.FAILED
+        if self.grid[src.y][src.x].state != player: return move_status.FAILED
+        if self.grid[dest.y][dest.x].state != tile_state.EMPTY: return move_status.FAILED
+
+        x_diff = src.x - dest.x
+        y_diff = src.y - dest.y
+        # is a one-space diagonal move
+        if abs(x_diff) == 1 and abs(y_diff) == 1:
+            return move_status.MOVED
+        # is a two-space diagonal move that captures
+        if abs(x_diff) == 2 and abs(y_diff) == 2:
+            y_offset = y_diff // 2
+            x_offset = x_diff // 2
+            if (self.grid[y_offset][x_offset].state != tile_state.EMPTY and 
+            self.grid[y_offset][x_offset].state != player):
+                return move_status.CAPTURED
+        # in all other cases the move is illegal
+        return move_status.FAILED
+
+    def make_move(self, src: point, dest: point, player: tile_state) -> move_status:
+        status = self.is_legal_move(src, dest, player)
+        if status == move_status.MOVED:
+            if src.self.grid[src.y][src.x].is_king:
+                self.grid[src.y][src.x].is_king = False
+                self.grid[dest.y][dest.x].is_king = True
+            self.grid[src.y][src.x].state = tile_state.EMPTY
+            self.grid[dest.y][dest.x] = player
+            # test for king transform
+            if (player == tile_state.PLAYER_1 and dest.y == 7):
+                self.grid[dest.y][dest.x].is_king = True
+            elif (player == tile_state.PLAYER_2 and dest.y == 0):
+                self.grid[dest.y][dest.x].is_king = True
+        elif status == move_status.CAPTURED:
+            x_diff = src.x - dest.x
+            y_diff = src.y - dest.y
+
+            y_offset = y_diff // 2
+            x_offset = x_diff // 2
+            self.grid[y_offset][x_offset].state = tile_state.EMPTY
+            self.grid[y_offset][x_offset].is_king = tile_state.EMPTY
+            if src.self.grid[src.y][src.x].is_king:
+                self.grid[src.y][src.x].is_king = False
+                self.grid[dest.y][dest.x].is_king = True
+            self.grid[src.y][src.x].state = tile_state.EMPTY
+            self.grid[dest.y][dest.x].state = player
+
+            # test for king transform
+            if (player == tile_state.PLAYER_1 and dest.y == 7):
+                self.grid[dest.y][dest.x].is_king = True
+            if (player == tile_state.PLAYER_2 and dest.y == 0):
+                self.grid[dest.y][dest.x].is_king = True
+
+        return status
+
+    def is_legal_capture_move(self, src: point, dest: point, player: tile_state):
+
+        return False
+
+    def make_capture_move(self, src: point, dest: point, player: tile_state):
+        status = self.is_legal_move(src, dest, player)
+        if status == move_status.CAPTURED:
+            x_diff = src.x - dest.x
+            y_diff = src.y - dest.y
+
+            y_offset = y_diff // 2
+            x_offset = x_diff // 2
+            self.grid[y_offset][x_offset].state = tile_state.EMPTY
+            self.grid[y_offset][x_offset].is_king = tile_state.EMPTY
+            self.grid[src.y][src.x].state = tile_state.EMPTY
+            self.grid[src.y][src.x].is_king = False
+            self.grid[dest.y][dest.x].state = player
+            if src.self.grid[src.y][src.x].is_king:
+                self.grid[src.y][src.x].is_king = False
+                self.grid[dest.y][dest.x].is_king = True
+
+            # test for king transform
+            if (player == tile_state.PLAYER_1 and dest.y == 7):
+                self.grid[dest.y][dest.x].is_king = True
+            if (player == tile_state.PLAYER_2 and dest.y == 0):
+                self.grid[dest.y][dest.x].is_king = True
+        return status
+    
+    # First move may be any move
+    # If first move captured a piece
+    #   make subsequent move
+    def make_moves():
+        return
+
+
+b = board()
+b.print()
--- bot.py
+++ bot.py
@@ -1,1 +1,4 @@
 # AI stuff with keras
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
